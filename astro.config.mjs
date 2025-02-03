781e54fd810c21098b6d728a702e462dc07b57e3
Vladimir Vasilevski <wasilevski@gmail.com>
2025-02-04 01:27:06 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,38 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
+import "@fontsource/ibm-plex-sans";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Behavix',
+          social: {
+              github: 'https://github.com/withastro/starlight',
+          },
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+          customCss: [
+              // Path to your custom CSS file
+              './src/styles/custom.css',
+          ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
